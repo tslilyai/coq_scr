11802aee67380ce4ad1df5e531b2324649547b1f
Lily Tsai <tslilyai@gmail.com>
2017-05-17 10:20:10 -0400

    define history_of_trace

--- definitions.v
+++ definitions.v
@@ -16,11 +16,8 @@ Definition tid : Type := nat.
 Inductive action : Type :=
 | Nat : nat -> action
 | Continue : action.
-Definition taction := tid -> action.
+Definition taction : Type := tid * action.
 Definition history : Type := list taction.
-
-(* Definition history_wf *)
-(* Definition history_reorder *)
 
 (* Does this work in place of memory? could also be a map *)
 Definition thread_history_state : Type := tid -> nat.
@@ -31,23 +28,32 @@ Inductive state :=
 | sht : thread_history_state -> state
 | scommutet : thread_commute_state -> state.
 
-Definition trace : Type := list (state * action).
-
-Parameter ref_impl : (refstate * action) -> (refstate * action).
+Parameter ref_impl : (refstate * taction) -> (refstate * taction).
 Parameter spec : list history.
 
-Definition history_of_trace : trace -> history.
-  Admitted.
+(* Definition history_wf *)
+(* Definition history_reorder *)
 
-Axiom ref_impl_correct : forall tr : trace,
-    In (history_of_trace tr) spec.
+Definition trace : Type := list ((state * taction) * (state * taction)).
+Function history_of_trace (tr: trace) : history :=
+  match tr with
+  | [] => []
+  | ((s1, a), (s2, r)) :: tl =>
+             match a,r with
+             | (t, Continue), (t', Continue) => history_of_trace tl
+             | (t, Continue), _ => r :: history_of_trace tl
+             | _, (t, Continue) => a :: history_of_trace tl
+             | _,_ => a :: r :: history_of_trace tl
+             end
+  end.
 
 Definition sim_commutes (tr : trace) : Prop.
 Admitted.
-
 Definition reordered (tr tr' : trace) : Prop.
 Admitted.
   
+Axiom ref_impl_correct : forall tr : trace,
+    In (history_of_trace tr) spec.
 Axiom SIM_reordered_histories_correct : forall tr tr0 tr1 tr2 tr1' : trace,
     tr = tr0 ++ tr1 ++ tr2 ->
     sim_commutes tr1 ->
